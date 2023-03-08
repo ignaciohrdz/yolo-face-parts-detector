6e4def52216990059fe2fd6946f29815a1cf95fd
Ignacio Hernández Montilla <ignaciohrdz@gmail.com>
2023-03-08 19:07:00 +0100

    Working on the detection script

--- detect.py
+++ detect.py
@@ -1,5 +1,8 @@
 from ultralytics import YOLO
 import os
+import cv2
+import supervision as spv
+
 
 if __name__ == "__main__":
 
@@ -7,5 +10,39 @@ if __name__ == "__main__":
 
     # Creating the model
     model = YOLO(os.path.join(path_model, "best.pt"))
-    results = model.predict(source="0", show=True)
+
+    # This will draw the detections
+    class_colors = spv.ColorPalette.from_hex(['#990099', '#ff66ff', '#00cc00', '#cc0000'])
+    box_annotator = spv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1,
+        color=class_colors
+    )
+
+    # Reading frames from the webcam
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        result = model(frame, agnostic_nms=True)[0]
+        detections = spv.Detections.from_yolov8(result)
+
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        frame = box_annotator.annotate(
+            scene=frame,
+            detections=detections,
+            labels=labels
+        )
+
+        cv2.imshow("Face parts", frame)
+        k = cv2.waitKey(1)
+
+        if k == ord("q"):
+            break
 
