40ffc378816e97c47080b327a86b48bb8bf60e8f
Ignacio Hernández Montilla <ignaciohrdz@gmail.com>
2023-05-04 20:09:01 +0200

    Working on multi GPU training - Changing train.py to train one model only

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from utils import *
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-a", '--arch', type=str, default='n', help="Architecture (n, s, m, l, x)")
     parser.add_argument("-d", '--data_dir', type=str, help="Path to the datasets folder")
     parser.add_argument("-b", '--batch_size', type=int, default=8, help="Batch size")
     parser.add_argument("-e", '--epochs', type=int, default=10, help="Number of epochs")
@@ -22,11 +23,10 @@ if __name__ == "__main__":
     path_yaml = os.path.join(path_face_parts, "split", "data.yaml")
 
     # Training all models
-    for m in ['n', 's', 'm', 'l', 'x']:
-        model = YOLO("weights/yolov8{}.pt".format(m))
+    model = YOLO("weights/yolov8{}.pt".format(args.arch))
 
-        # Train the model
-        results = model.train(data=path_yaml, task="detect", name="train_{}".format(m),
-                              epochs=args.epochs, batch=args.batch_size,
-                              device=",".join(args.device),
-                              scale=0.25, degrees=25.0, mosaic=0.8)
+    # Train the model
+    results = model.train(data=path_yaml, task="detect", name="train_{}".format(args.arch),
+                          epochs=args.epochs, batch=args.batch_size,
+                          device=",".join(args.device),
+                          scale=0.25, degrees=25.0, mosaic=0.8)
